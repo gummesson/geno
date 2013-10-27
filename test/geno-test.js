e5e8b471d5514ba156822bb6a8affa15084f54f0
Ellen Gummesson <ellengummesson@gmail.com>
2013-10-27 13:58:22 +0100

    Add 'assets'.

--- test/geno-test.js
+++ test/geno-test.js
@@ -13,7 +13,6 @@ describe('Geno', function() {
     it('must build the site', function(done) {
       geno.build(function() {
         fsExists('site/index.html').must.be.true();
-        fsExists('site/style.css').must.be.true();
         done();
       });
     });
@@ -21,13 +20,20 @@ describe('Geno', function() {
     // Since `nunjucks` affects the whitespace formatting 
     // the test unfortunately has to check for template tags 
     // instead of comparing the actual content.
-    it('must build all of the content', function(done) {
+    it('must generate the content', function(done) {
       fs.readFile('site/index.html', {encoding: 'utf8'}, function(err, data) {
         var pattern = /\{(\{|%-?)\s+.*\s+(-?%|\})\}/g,
             tags    = pattern.test(data);
         if (err) { throw err; }
         data.must.exist();
         tags.must.be.false();
+        done();
+      });
+    });
+
+    it('must copy the assets', function(done) {
+      geno.build(function() {
+        fsExists('site/style.css').must.be.true();
         done();
       });
     });
